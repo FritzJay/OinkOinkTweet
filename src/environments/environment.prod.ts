7465dd85c38c057bc076c6812e97a04e655104f7
Jonathan F. Jay <Jonathan.jay10@gmail.com>
2017-05-10 15:16:27 -0700

    Added valid url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   urls: [
-    'http://localhost:3000/twitter/',
+    'https://oinkoinktweetapi.herokuapp.com/twitter/',
     'http://localhost:3000/db/'
   ]
 };
